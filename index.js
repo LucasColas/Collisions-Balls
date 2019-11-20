274e8e4a5eea2efbe48134e674da404d687d13bf
MrRex <51634013+MrRex42@users.noreply.github.com>
2019-11-20 18:33:25 +0100

    Update index.js
    I changed the syntax of the code. I use the new syntax of JavaScript.

--- index.js
+++ index.js
@@ -1,112 +1,145 @@
-var balls = [];
-var nbBalles = 42;
-var VMAX = 15;
 
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
+var canvas, context, frametime, before = Date.now(), now;
 
-var width = canvas.width = window.innerWidth;
-var height = canvas.height = window.innerHeight;
+var nbBalles = 50;
+var Balls = [];
+var speed = 200;
+var radiusc = 20;
 
 
-function random(min,max) {
-  var num = Math.floor(Math.random()*(max-min)) + min;
-  return num;
+window.onload = function() {
+	canvas = document.getElementById("canvas1");
+    context = canvas.getContext("2d");
+    addBall();
+    animate();
+
+    var interval = this.setInterval(this.resizeCanvas, 1000);
+
 }
 
 
 
-function Ball(x, y, velX, velY, color, size) {
-  this.x = x;
-  this.y = y;
-  this.velX = velX;
-  this.velY = velY;
-  this.color = color;
-  this.size = size;
+function animate() {
+	FPS();
+
+	context.clearRect(0, 0, canvas.width, canvas.height);
+
+	CheckCollision();
+
+	animateBalls();
+	requestAnimationFrame(animate);
 }
 
-
-Ball.prototype.draw = function() {
-  ctx.beginPath();
-  ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-  ctx.fill();
-};
-
-
-Ball.prototype.update = function() {
-  if((this.x + this.size) >= width) {
-    this.velX = -(this.velX);
-  }
-  else if((this.x - this.size) <= 0) {
-    this.velX = -(this.velX);
-  }
-
-  if((this.y + this.size) >= height) {
-    this.velY = -(this.velY);
-  }
-  else if((this.y - this.size) <= 0) {
-    this.velY = -(this.velY);
-  }
-
-  this.x += this.velX;
-  this.y += this.velY;
-};
-
-
-Ball.prototype.collisionDetect = function(balle2) {
-      var dx = this.x - balle2.x;
-      var dy = this.y - balle2.y;
-      var distance = Math.sqrt(dx * dx + dy * dy);
-
-      if (distance < this.size + balle2.size) {
-        return (true)
-      }
-};
-
-
- 
-
-while(balls.length < nbBalles) {
-  var size = random(10,20);
-  var ball = new Ball(
-
-    random(0 + size,width - size),
-    random(0 + size,height - size),
-    random(-VMAX,VMAX),
-    random(-VMAX,VMAX),
-    'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')',
-    size
-  );
-  balls.push(ball);
+function FPS() {
+	now = Date.now();
+	frametime = (now-before) / 1000;
+	before = now;
 }
 
 
 
-function loop() {
-  ctx.fillStyle = 'rgba(0,0,0)';
-  ctx.clearRect(0,0,width,height);
+class Ball {
 
-  for(var i = 0; i < balls.length; i++) {
-    for(var j = i+1 ; j < balls.length ; j++)
-    {
-      if (balls[i].collisionDetect(balls[j]))
-      {
-        
-      balls.splice(i,1)
-      balls.splice(j,1)
-      }    
-      else
-      {
-        balls[i].draw();
-        balls[i].update();
-      }
-    }
-  }
+	constructor(x, y, vx, vy, radius, r, g, b) {
+		this.x = x;
+		this.y = y;
+		this.vx = vx;
+		this.vy = vy;
+		this.radius = radius; 
+		this.r = r;
+		this.g = g;
+		this.b = b;
 
-  requestAnimationFrame(loop);
+	}
+
+	Draw() {
+		this.x = this.x + this.vx*frametime;
+		this.y = this.y + this.vy*frametime;
+		context.fillStyle = "rgb(" + this.r + ", " + this.g + ", " + this.b + ")"
+		context.arc(this.x, this.y, this.radius, 0, Math.PI*2);
+		context.closePath();
+		context.fill();
+
+
+		if(this.vx > 0 ) {
+            if(this.x + this.radius >= canvas.width) {
+                this.vx = -this.vx;
+            }
+        } else {
+            if(this.x - this.radius <= 0) {
+                this.vx = -this.vx;
+            }
+        }
+
+        if (this.vy > 0 ) {
+            if(this.y + this.radius >= canvas.height) {
+                this.vy = -this.vy;
+            }
+        } else {
+            if(this.y - this.radius <= 0) {
+                this.vy = -this.vy;
+            }
+        }
+	}
+
+	Distance(ball) {
+		var distance = Math.pow(this.x - ball.x, 2) + Math.pow(this.y - ball.y, 2)
+		return Math.sqrt(
+			distance
+			)
+	}	
+
 }
 
 
 
-loop();
+function animateBalls() {
+	for (i = 0; i <= Balls.length-1 ; i++) {
+		Balls[i].Draw();
+	}
+
+}
+
+
+function resizeCanvas () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+} 
+
+function addBall() {
+	for (var j = 0; j < nbBalles ; j++) {
+		Balls.push(new Ball
+			(
+			Math.random()*canvas.width,
+			Math.random()*canvas.height,
+			(Math.random()+0,2)*speed,
+			(Math.random()+0,2)*speed,
+			(Math.random()+0,5)*radiusc,
+			Math.random()*255,
+			Math.random()*255,
+			Math.random()*255,
+			))
+
+	}
+}
+
+function CheckCollision() {
+
+	for(i = 0; i <= Balls.length-1; i++) {
+		for(j = 0; j <= Balls.length-1; j++) {
+			var distance = Balls[j].Distance(Balls[i]);
+
+			if (distance < Balls[j].rayon + Balls[i].rayon && Balls[i] != Balls[j]) {
+				if (i > j) {
+					Balls.splice(i, 1);
+					Balls.splice(j, 1);
+
+				} else {
+					Balls.splice(j, 1);
+					Balls.splice(i, 1);
+				}
+			}
+		}
+	}
+
+}
